58d8f152f8bc4b8cde2fc4b7c413852e401b5d90
HyperCreatorTM <sgerhri2@gmail.com>
2026-01-16 20:26:18 +0300

    feat: Luxury Matte UI Redesign & GitHub Actions

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/SubSettingActivity.kt
@@ -0,0 +1,91 @@
+package com.v2ray.ang.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.v2ray.ang.R
+import com.v2ray.ang.databinding.ActivitySubSettingBinding
+import com.v2ray.ang.extension.toastError
+import com.v2ray.ang.extension.toastSuccess
+import com.v2ray.ang.handler.AngConfigManager
+import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
+import com.v2ray.ang.viewmodel.SubscriptionsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SubSettingActivity : BaseActivity() {
+    private val binding by lazy { ActivitySubSettingBinding.inflate(layoutInflater) }
+
+    private val viewModel: SubscriptionsViewModel by viewModels()
+    private lateinit var adapter: SubSettingRecyclerAdapter
+    private var mItemTouchHelper: ItemTouchHelper? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(binding.root)
+        setContentViewWithToolbar(binding.root, showHomeAsUp = true, title = getString(R.string.title_sub_setting))
+
+        adapter = SubSettingRecyclerAdapter(this, viewModel)
+
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        addCustomDividerToRecyclerView(binding.recyclerView, this, R.drawable.custom_divider)
+        binding.recyclerView.adapter = adapter
+
+        mItemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
+        mItemTouchHelper?.attachToRecyclerView(binding.recyclerView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        refreshData()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.action_sub_setting, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.add_config -> {
+            startActivity(Intent(this, SubEditActivity::class.java))
+            true
+        }
+
+        R.id.sub_update -> {
+            showLoading()
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                val count = AngConfigManager.updateConfigViaSubAll()
+                delay(500L)
+                launch(Dispatchers.Main) {
+                    if (count > 0) {
+                        toastSuccess(R.string.toast_success)
+                        refreshData()
+                    } else {
+                        toastError(R.string.toast_failure)
+                    }
+                    hideLoading()
+                }
+            }
+
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun refreshData() {
+        viewModel.reload()
+        adapter.notifyDataSetChanged()
+    }
+}
